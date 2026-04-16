c6afdc978142b683b86edf318f43780e890f9f12
nurmeldayanti1167 <nurmeldayanti58@gmail.com>
2026-04-16 08:48:50 +0800

    commit 4

--- mel.py
+++ mel.py
@@ -5,3 +5,6 @@ while True:
     if nama.lower() == 'selesai': break
     peserta.append(nama)
     random.shuffle(peserta)
+    tengah = len(peserta) // 2
+tim_a = peserta[:tengah]
+tim_b = peserta[tengah:]
